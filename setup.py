8c31368fb49af8f50b6715d4ed702c6792dfc566
Takeru Soma <vxjp4649tkeru@icloud.com>
2023-11-29 19:19:56 +0900

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os         
+from glob import glob
 from setuptools import setup
 
 package_name = 'mypkg'
@@ -10,16 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='takeru',
+    maintainer='Takeru Soma',
     maintainer_email='vxjp4649tkeru@icloud.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='a package for practice',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
