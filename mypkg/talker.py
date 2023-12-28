c59b29f2336d2bad3ef4ee988dbd529a7b577ee4
Takeru Soma <vxjp4649tkeru@icloud.com>
2023-12-28 14:00:45 +0900

    update

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,13 +3,21 @@ from rclpy.node import Node
 from person_msgs.msg import Int16 
 
 class Talker():
-    def __init__(self): 
+    def __init__(self,node): 
         self.pub = node.create_publisher(Int16,"countup",10)
         self.n = 0
+        node.create_timer(0.5, self.cb)
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 rclpy.init()
 node = Node("talker")
-talker = Talker()
+talker = Talker(node)
+rclpy.spin(node)
 
 def cb (self):
     msg = Int16()
