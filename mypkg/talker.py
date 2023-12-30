375d66a450fd1b45058f3091b8ac67004d949ae0
Takeru Soma <vxjp4649tkeru@icloud.com>
2023-12-31 05:01:33 +0900

    fix

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,27 +1,21 @@
 #SPDX-FileCopyrightText: 2023 Takeru Soma
 #SPDX-License-Indentifier: BSD-3-Clause
 
-import rclpy
-from rclpy.node import Node
-from person_msgs.msg import Int16 
+import rclpy                     
+from rclpy.node import Node     
+from std_msgs.msg import Int16 
 
-class Talker():
-    def __init__(self,node): 
-        self.pub = node.create_publisher(Int16,"countup",10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
+rclpy.init()
+node = Node("talker")            
+pub = node.create_publisher(Int16, "countup", 10) 
+n = 0 
 
-    def cb(self):
-        msg = Int16()
-        msg.data = self.n
-        self.pub.publish(msg)
-        self.n += 1
+def cb():          
+global n       
+msg = Int16()  
+msg.data = n  
+pub.publish(msg)   
+n += 1
 
-def main():
-    rclpy.init()
-    node = Node("talker")
-    talker = Talker(node)
-    rclpy.spin(node)
-
-if __name__ == '__main__':
-    main()
+node.create_timer(0.5, cb) 
+rclpy.spin(node)
